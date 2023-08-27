a4f942294deaf56155b08abbf24fb4dbacec0293
RooT0616 <matumokencave@gmail.com>
2023-08-27 15:13:42 +0900

    改善

--- Dicebot.py
+++ Dicebot.py
@@ -2,18 +2,21 @@ import random
 import discord
 import json
 import logging
-from discord.ext import commands
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
 
 # Read configuration from an external file
-with open('/home/game/Dicebot/config.json', 'r') as file:
+with open('config.json', 'r') as file:
     config = json.load(file)
 
 # Constants
 BOT_TOKEN = config["TOKEN"]
-HELP_MESSAGE = config["HELP_MESSAGE"]  # Assumed to be defined in the config.json
+HELP_MESSAGE = config["HELP_MESSAGE"]
+MAX_FACES = config["MAX_FACES"]
+MIN_FACES = config["MIN_FACES"]
+MAX_ROLLS = config["MAX_ROLLS"]
+MIN_ROLLS = config["MIN_ROLLS"]
 
 # Helper Functions
 def format_dice_result(result, faces, times):
@@ -22,21 +25,21 @@ def format_dice_result(result, faces, times):
 async def send_dice_result(message, faces, times):
     result = [str(random.randint(1, faces)) for _ in range(times)]
     await message.channel.send('<@' + str(message.author.id) + '>' + format_dice_result(result, faces, times))
-    logging.info('@' + str(message.author) + format_dice_result(result, faces, times))
+    logging.info('@' + str(message.author) + ' ' + format_dice_result(result, faces, times))
 
 async def send_error(message, error_message):
     await message.channel.send('<@' + str(message.author.id) + '>' + f"エラー: {error_message}")
-    logging.error('@' + str(message.author) + f"エラー: {error_message}")
+    logging.error('@' + str(message.author) + ' ' + f"エラー: {error_message}")
 
 # Command Handlers
 async def handle_d_command(message, faces, default_times=1):
     times_str = message.content.split(' ')[-1]
     try:
         times = int(times_str) if times_str.isdigit() else default_times
-        if 1 <= times <= 10:
+        if MIN_ROLLS <= times <= MAX_ROLLS:
             await send_dice_result(message, faces, times)
         else:
-            await send_error(message, '1から10の回数で振ってください')
+            await send_error(message, f'回数は{MIN_ROLLS}から{MAX_ROLLS}で指定してください')
     except ValueError:
         await send_error(message, '無効な回数です')
 
@@ -44,30 +47,12 @@ async def handle_custom_roll_command(message):
     try:
         parts = message.content.split(' ')
         times, faces = map(int, parts[1].split('d'))
-        if 1 <= times <= 10 and 2 <= faces <= 100:
+        if MIN_ROLLS <= times <= MAX_ROLLS and MIN_FACES <= faces <= MAX_FACES:
             await send_dice_result(message, faces, times)
         else:
-            await send_error(message, '回数は1から10、面数は2から100で指定してください')
+            await send_error(message, f'回数は{MIN_ROLLS}から{MAX_ROLLS}、面数は{MIN_FACES}から{MAX_FACES}で指定してください')
     except ValueError:
         await send_error(message, '無効なコマンド形式です。使用方法: /r 回数d面数')
-
-async def handle_command(message):
-    if message.author == client.user:
-        return  # Ignore messages from the bot itself
-    content = message.content
-    if content.startswith("/dhelp"):
-        await message.channel.send('<@' + str(message.author.id) + '>' + HELP_MESSAGE)
-        logging.info('@' + str(message.author) + HELP_MESSAGE)
-    elif content.startswith("/d1"):
-        await handle_d_command(message, 6, 1)
-    elif content.startswith("/d2"):
-        await handle_d_command(message, 6, 2)
-    elif content.startswith("/d3"):
-        await handle_d_command(message, 6, 3)
-    elif content.startswith("/d00"):
-        await handle_d_command(message, 100, 1)
-    elif content.startswith("/r "):
-        await handle_custom_roll_command(message)
 
 # Main
 intents = discord.Intents.default()
--- README.md
+++ README.md
@@ -1,28 +1,65 @@
+# README.md
 
-# Dicebot
+## English
 
-Dicebotは、Discord上で様々なサイコロを振ることができるシンプルなボットです。
+### Dicebot for Discord
 
-## 機能
+#### Introduction
+This Python script serves as a dice-rolling bot for Discord servers. Users can roll dice with various faces and times based on the command provided.
 
-- 6面のサイコロを1から3回振ることができます（`/d1`, `/d2`, `/d3`）。
-- 100面のサイコロを1回振ることができます（`/d00`）。
-- 3面のサイコロを1回振ることができます（`/1d3`）。
-- 任意の回数と面数でサイコロを振ることができます（`/roll 回数d面数`）。
+#### Features
+- Roll a 6-sided dice with commands like `/d1`, `/d2`, `/d3`.
+- Roll a 100-sided dice with the command `/d00`.
+- Custom roll with the command `/r` followed by the format `XdY` where `X` is the number of rolls and `Y` is the number of faces.
+- Error handling to guide users on proper usage.
 
-## 使用方法
+#### Requirements
+- Python 3.x
+- Discord.py library
+- JSON for configuration
 
-1. ボットをサーバーに追加します。
-2. テキストチャンネルで上記のコマンドを使用します。
-3. ボットがサイコロの結果を返します。
+#### Installation
+1. Clone the repository.
+2. Install the required packages.
+3. Update the `config.json` with your Discord bot token and other settings.
+4. Run the script.
 
-## インストール
+#### Configuration
+The `config.json` file contains settings like:
+- Discord bot token
+- Help message
+- Max and Min faces for dice
+- Max and Min rolls for dice
 
-1. Python 3.8以上が必要です。
-2. 必要な依存関係をインストールします：`pip install discord.py`
-3. 設定ファイルにDiscordボットトークンを追加します。
-4. `Dicebot.py`を実行します。
+---
 
-## ライセンス
+## 日本語
 
-このプロジェクトはMITライセンスのもとで公開されています。
+### Discord用ダイスボット
+
+#### 紹介
+このPythonスクリプトは、Discordサーバー用のダイスを振るボットです。ユーザーは提供されたコマンドに基づいて、さまざまな面と回数でダイスを振ることができます。
+
+#### 機能
+- `/d1`、`/d2`、`/d3`などのコマンドで6面のサイコロを振る。
+- `/d00`のコマンドで100面のサイコロを振る。
+- `/r`コマンドに続いて `XdY` の形式でカスタムロールができる。ここで `X` は回数、`Y` は面数。
+- 適切な使用方法についてユーザーをガイドするエラーハンドリング。
+
+#### 必要条件
+- Python 3.x
+- Discord.pyライブラリ
+- 設定用のJSON
+
+#### インストール方法
+1. リポジトリをクローンする。
+2. 必要なパッケージをインストールする。
+3. `config.json`にDiscordボットのトークンと他の設定を更新する。
+4. スクリプトを実行する。
+
+#### 設定
+`config.json` ファイルには以下のような設定が含まれます：
+- Discordボットトークン
+- ヘルプメッセージ
+- サイコロの最大・最小面数
+- サイコロの最大・最小振り数
